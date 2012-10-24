27433a417a213f7d160bf408a35dc959177e9cc3
Neal Ehardt <nealehardt@gmail.com>
2012-10-23 19:25:41 -0500

    Set it off

---
+++ shared/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  Colorful
+//
+//  Created by groovebug on 10/23/12.
+//  Copyright (c) 2012 groovebug. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+{
+    IBOutlet UIWebView *webView;
+}
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"config" ofType:@"plist"];
+    NSDictionary *plistDict = [NSDictionary dictionaryWithContentsOfFile:plistPath];
+    
+    NSURL *url = [NSURL URLWithString:[plistDict objectForKey:@"homeURL"]];
+    NSLog(@"\npath: %@\ndict: %@\nurl: %@", plistPath, plistDict, url);
+    [webView loadRequest:[NSURLRequest requestWithURL:url]];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
